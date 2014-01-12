4020206ceb255d0d3dbbb64d4ff1c44031647975
android <trevd1234@gmail.com>
2014-01-12 02:59:38 +0000

    Add static library building

--- Android.mk
+++ Android.mk
@@ -1,10 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 
-include $(CLEAR_VARS)
-
-#LOCAL_ARM_MODE := arm
-
-LOCAL_SRC_FILES:= \
+libusb1_src_files:= \
 	libusb/core.c \
 	libusb/descriptor.c \
 	libusb/io.c \
@@ -12,21 +8,32 @@ LOCAL_SRC_FILES:= \
 	libusb/os/linux_usbfs.c
 
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/android \
+libusb1_includes += $(LOCAL_PATH)/android \
 	$(LOCAL_PATH)/libusb \
 	$(LOCAL_PATH)/libusb/os 
-
-LOCAL_CFLAGS += -W -Wall
-LOCAL_CFLAGS += -fPIC -DPIC
-
-
+    
+libusb1_cflags := -W -Wall -fPIC -DPIC
 ifeq ($(TARGET_BUILD_TYPE),release)
-	LOCAL_CFLAGS += -O2
+    libusb1_cflags += -O2
 endif
 
-LOCAL_MODULE:= libusb
+
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := $(libusb1_cflags)
+LOCAL_C_INCLUDES := $(libusb1_includes)
+LOCAL_SRC_FILES := $(libusb1_src_files)
+LOCAL_MODULE := libusb-1.0
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false 
 include $(BUILD_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
 
+LOCAL_CFLAGS := $(libusb1_cflags)
+LOCAL_C_INCLUDES := $(libusb1_includes)
+LOCAL_SRC_FILES := $(libusb1_src_files)
+LOCAL_MODULE := libusb-1.0
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRELINK_MODULE := false 
+include $(BUILD_STATIC_LIBRARY)
